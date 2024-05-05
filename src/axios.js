1207e9dbcbbe805180fe92a9dc43889dd4f15989
sachin krupal uke <sachinkrupaluke@sachins-MacBook-Air.local>
2024-05-05 10:36:39 +0530

    function deployed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://127.0.0.1:5001/woodzone-12dca/us-central1/api'         //url of an api{Cloud function}
+    baseURL:'https://wood-zone-backend.vercel.app/'         //url of an api{Cloud function}
 })
 //http://127.0.0.1:5001/woodzone-12dca/us-central1/api
 //http://127.0.0.1:5001/woodzone-12dca/us-central1/api
